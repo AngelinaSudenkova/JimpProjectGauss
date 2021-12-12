712533b1af4f23cb5dfc9104e0b0e4b40413650b
Sebastian Kałuziński <skaluzinski1801@gmail.com>
2021-12-12 18:48:52 +0100

    Dodanie podstawiania wstecznego oraz partial pivoting

--- czytstruct.c
+++ czytstruct.c
@@ -6,7 +6,28 @@
 //     double *a; //macierz
 //     double *b; //wektor prawych stron
 // } *ur_t;
+void changeRows(double *rowA,double *rowB,int n){
+    int i;
+    double temp;
+    for( i = 0; i<n ; i++ ) {
+        temp = rowA[i] ;
+        rowA[i] = rowB[i] ;
+        rowB[i] = temp;
+    }
+}
 
+void partialPivoting( double **A,int n){
+    int i,k;
+    for( i=0; i<n-1; i++ ) {
+        if( A[i][i]==0 ){
+            for( k=0; k<n; k++ ) {
+                if( A[k][i] != 0 ) {
+                    changeRows( A[k], A[i],n );
+                }
+            }
+        }
+    }
+}
 
 struct Gauss_system{
     int n; 
@@ -68,19 +89,56 @@ ur_t czytaj(char *nazwa_pliku){
 }
 
 int main ( int argc, char **argv){
-
+    double suma;
+    
    ur_t u = czytaj(argv[1]);
-
+    printf("Macierz A:\n");
     for(int i = 0; i < u->n; i++){
         for(int j = 0; j < u->n; j++ ){
-            printf( "%lf ", u->a[i][j]);
+            printf( "%.2lf ", u->a[i][j]);
         }
         printf("\n");
     }
-
+    printf("Macierz B\n");
      for(int i = 0; i < u->n; i++){
-        printf("%lf ", u->b[i]);
+        printf("%.2lf ", u->b[i]);
     }
+    
+    double **A = u->a;
+    double *b = u->b;
+    int n = u->n;
+    double *x = malloc(sizeof(double) * n);
+   for ( int k = 0; k < n-1; k++){
+       for(int w = k+1 ; w < n; w++){
+           double q = A[w][k]/ A[k][k];
+           for(int j = 0; j < n; j++){
+               A[w][j] -= A[k][j]*q; 
+           }
+       }
+    x[n-1] = b[n-1] / a[n-1][n-1];
+    for(int i=n-2;i>-1;i--){
+        suma = 0;
+        for(int j=i+1;j<n;j++){
+            suma += a[i][j] * x[j];
+        }
+        x[i]= (b[i] - suma) / a[i][i]; 
+    }
+    printf("[ ");
+    for(int i = 0;i<n;i++){
+        printf("%lf ",x[i]);
+    }
+    printf("]");  
+//     printf("Nowa\n");
+//    for(int i = 0; i < u->n; i++){
+//        printf("\n");
+//         for(int j = 0; j < u->n; j++ ){
+//             printf( "%.2lf ", u->a[i][j]);
+//         }
+//    }
+//     printf("\n");
+//     for(int i = 0; i < u->n; i++){
+//         printf("%.2lf ", u->b[i]);
+//     }
 
     free(u->b);
     for ( int i = 0; i < u->n; i++){
