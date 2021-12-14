f87ab53cfd7fd86c1b2af99ac7a8680664a0b9d7
AngelinaSudenkova <sudenkova.anhelina@gmail.com>
2021-12-14 14:31:05 +0100

    Wysylam ostatnia wesje plikow

--- czytstruct.c
+++ czytstruct.c
@@ -1,11 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-// typedef struct{
-//     int n; //liczba rownan
-//     double *a; //macierz
-//     double *b; //wektor prawych stron
-// } *ur_t;
+
 void changeRows(double *rowA,double *rowB,int n){
     int i;
     double temp;
@@ -34,10 +30,9 @@ struct Gauss_system{
     double **a; 
     double *b; 
 };
-// typedef double real;
+
 typedef struct Gauss_system *ur_t;
-// ur_t u;
-// struct Gauss_system *uvar2;
+
 
 
 ur_t czytaj(char *nazwa_pliku){
@@ -63,20 +58,9 @@ ur_t czytaj(char *nazwa_pliku){
         }
     }
 
-    //  for(int i = 0; i < size; i++){
-    //     for(int j = 0; j <size; j++ ){
-    //         printf( "%lf ", matrix[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-
     for(int i = 0; i < size; i++){
         fscanf(in, "%lf", vector+i);
     }
-
-    // for(int i = 0; i < size; i++){
-    //     printf("%lf ", vector[i]);
-    // }
 
     ur_t u = malloc(sizeof(struct Gauss_system));
     u->n = size;
@@ -133,24 +117,17 @@ int main ( int argc, char **argv){
             suma += A[i][j] * x[j];
         }
         x[i]= (b[i] - suma) / A[i][i]; 
-        printf("X wynosi %lf\n",x[i]);
+        printf("\n");
+        printf("\nX wynosi %lf\n",x[i]);
     }
+
+
     printf("[ ");
-    for(i = 0;i<n;i++){
+    for(i = 0; i < n ; i++){
         printf("%lf ",x[i]);
     }
     printf("]"); 
-//     printf("Nowa\n");
-//    for(int i = 0; i < u->n; i++){
-//        printf("\n");
-//         for(int j = 0; j < u->n; j++ ){
-//             printf( "%.2lf ", u->a[i][j]);
-//         }
-//    }
-//     printf("\n");
-//     for(int i = 0; i < u->n; i++){
-//         printf("%.2lf ", u->b[i]);
-//     }
+
 
     free(u->b);
     for ( int i = 0; i < u->n; i++){
